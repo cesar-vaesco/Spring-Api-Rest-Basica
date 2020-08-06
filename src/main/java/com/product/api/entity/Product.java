9c9db00522ef573d0a8892f5829b79e9ea90e1e6
Cesar <cesarvargasescorcia@gmail.com>
2020-08-06 09:56:33 -0500

    Método Post - Método Delete: Guardando nuevo producto y eliminando producto

--- src/main/java/com/product/api/entity/Product.java
+++ src/main/java/com/product/api/entity/Product.java
@@ -14,6 +14,7 @@ import javax.persistence.Table;
 @Table(name= "products")
 public class Product implements Serializable {
 
+
 	@Id
 	@Column(name="id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -40,5 +41,6 @@ public class Product implements Serializable {
 		this.name = name;
 	}
 	
-	
+
+	private static final long serialVersionUID = 1L;
 }
--- src/main/java/com/product/api/rest/ProductRest.java
+++ src/main/java/com/product/api/rest/ProductRest.java
@@ -5,8 +5,11 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,32 +23,46 @@ public class ProductRest {
 
 	@Autowired
 	private ProductsDAO productDAO;
-	
-	/*Mostrando todos los productos*/
-	@GetMapping("allProducts")
-	public ResponseEntity<List<Product>> getProducts(){
+
+	/* Mostrando todos los productos */
+	@GetMapping("allProducts") // url de respuesta :8080/products/allProducts
+	public ResponseEntity<List<Product>> getProducts() {
 		List<Product> products = productDAO.findAll();
 		return ResponseEntity.ok(products);
-	} 
-	
-	/*Mostrando producto usando id*/
-	//
-	@GetMapping("{productId}")
-	//@RequestMapping(value = "{productId}")
-	public ResponseEntity<Product> getProductById(@PathVariable("productId") Long productId){
-		       Optional<Product> optionalProduct = productDAO.findById(productId);
-		       if(optionalProduct.isPresent()) {
-		    	   /*Si encuentra el producto lo muestra */
-		    	   return ResponseEntity.ok(optionalProduct.get());
-		       } else {
-		    	   /*Si no encuentra el producto responde con not content*/
-		    	   return ResponseEntity.noContent().build();
-		       }
 	}
-	
-	
-	//@GetMapping
-	@RequestMapping(value="hello", method = RequestMethod.GET)
+
+	/* Mostrando producto usando id */
+	// url de respuesta :8080/products/{id}
+	@GetMapping("{productId}")
+	// @RequestMapping(value = "{productId}")
+	public ResponseEntity<Product> getProductById(@PathVariable("productId") Long productId) {
+		Optional<Product> optionalProduct = productDAO.findById(productId);
+		if (optionalProduct.isPresent()) {
+			/* Si encuentra el producto lo muestra */
+			return ResponseEntity.ok(optionalProduct.get());
+		} else {
+			/* Si no encuentra el producto responde con not content */
+			return ResponseEntity.noContent().build();
+		}
+	}
+
+	// Guardar un nuevo producto -> url de respuesta :8080/products
+	@PostMapping
+	public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+		Product newProduct = productDAO.save(product);
+		return ResponseEntity.ok(newProduct);
+	}
+
+	// Borrar un product -> url de respuesta :8080/products
+	@DeleteMapping(value= "{productId}")
+	public ResponseEntity<Void> deleteProduct(@PathVariable("productId") Long productId) {
+		productDAO.deleteById(productId);
+		return ResponseEntity.ok(null);
+	}
+
+	// url de respuesta :8080/products/hello
+	// @GetMapping
+	@RequestMapping(value = "hello", method = RequestMethod.GET)
 	public String hello() {
 		return "Hello World";
 	}
